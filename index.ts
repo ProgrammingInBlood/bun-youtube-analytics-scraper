2fc595e39792c4889edea106adc1ac920f73463c
Eklavya Raj <eklavyasingh12065@gmail.com>
2025-07-19 21:24:25 +0530

    fixed cors

--- index.ts
+++ index.ts
@@ -29,11 +29,6 @@ async function getDebugInfo() {
 }
 
 const app = new Elysia()
-  .use(cors({
-    origin: "*",
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    credentials: false
-  }))
   .get('/', () => 'YouTube Live Chat Aggregator API')
   .get('/debug', async () => {
     const debugInfo = await getDebugInfo();
